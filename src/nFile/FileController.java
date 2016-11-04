2afbbe192767f2dc25d5d0c8b111c718e6b2a0c4
dhina4792 <dhina4792@hotmail.com>
2016-11-04 11:52:11 +0800

    Virus Scan update

--- src/nFile/FileController.java
+++ src/nFile/FileController.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import org.json.JSONException;
+
 import fileScan.FileScan;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -31,29 +33,41 @@ public class FileController implements Initializable {
 	private ListView<Account> listView;
 
 	final FileChooser fileChooser = new FileChooser();
+	private Stage app_stage;
 
 	public void handleLogoutBtn(ActionEvent event) throws IOException {
 
-		//TODO: Proper Logging out of Account, Removing of Token
+		// TODO: Proper Logging out of Account, Removing of Token
 
 		Parent FilePageParent = FXMLLoader.load(getClass().getResource("/nLogin/Login.fxml"));
 		Scene FilePageScene = new Scene(FilePageParent);
-		Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		app_stage.setScene(FilePageScene);
 		app_stage.show();
 	}
 
 	public void handleUploadButton(ActionEvent event) throws IOException {
 
-//		// TODO:find out what's the stage here.is it primarystage or app_stage or filepagescene
-//		File file = fileChooser.showOpenDialog(stage);
-//		
-//		//once file is chosen,put in the filescan parameter
-//		if (file != null) {
-//			FileScan filescan = new FileScan(file);
-//			//this result determines whether the file has a virus or not
-//			System.out.println(filescan.isFileInfected());
-//		}
+		// opens up file dialog for user to choose
+		File file = fileChooser.showOpenDialog(app_stage);
+
+		// check if valid file
+		if (file != null) {
+			try {
+				// does the virus scan
+				FileScan filescan = new FileScan(file);
+				// this result determines whether the file has a virus or not
+				if (!filescan.isFileInfected()) {
+					// TODO: if file is not infected do necessary
+				} else {
+					System.out.println("File is virus infected");
+				}
+			} catch (Exception e) {
+				System.out.println("Invalid File");
+			}
+		} else {
+			System.out.println("Invalid File");
+		}
 
 	}
 
