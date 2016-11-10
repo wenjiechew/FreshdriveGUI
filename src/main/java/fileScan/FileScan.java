0cee6d91fc86617fd1a3a170b8d8dfb6076252e1
dhina4792 <dhina4792@hotmail.com>
2016-11-10 15:22:26 +0800

    Added comments for filescan

--- src/main/java/fileScan/FileScan.java
+++ src/main/java/fileScan/FileScan.java
@@ -1,3 +1,13 @@
+/**
+* This file scan class scans a file and informs the user if the file is infected.
+* This class was built on the VirusTotal Public API v2.0(https://www.virustotal.com/en/documentation/public-api/)
+* Any references to the  API is described more in the link above
+*
+* @author  Dhinakaran
+* @version 1.0
+* @since   2016-nov-03 
+*/
+
 package fileScan;
 
 import java.io.IOException;
@@ -19,12 +29,33 @@ import org.apache.http.client.methods.HttpPost;
 
 public class FileScan {
 
+	// var to store whether a file is infected(true means file is infected false
+	// means its false)
 	private static boolean fileInfected;
+	// the file object which requires to be scanned
 	private static File fileToScan;
+	// this var is the id of the file when sent to the api for scanning
 	private static String resource;
+	// this var is response code of the reply coming from the api
 	private static int responseStatus;
-	
 
+	// api for the file scan
+	private static final String URL_FILESCAN = "https://www.virustotal.com/vtapi/v2/file/scan";
+	// api for the file scan report
+	private static final String URL_FILEREPORT = "https://www.virustotal.com/vtapi/v2/file/report";
+	// the key used when making a api call
+	private static final String API_KEY = "8eae5cfab4d8d18cd4a15b1aac7dbd5710558e2667a8394805160f819896a3b4";
+
+	/**
+	 * FileScan constructor method.All the scanning starts here First the file
+	 * which is passed in is being assigned Next it runs the
+	 * scanFile,responsestatus and scanresults methods there is thread sleep
+	 * here to wait for 1 minute before making an API call again to get the scan
+	 * results. The reason is public API key is only allowed 4 requests/minute
+	 *
+	 * @param file 		file object which is going to be scanned
+	 * @return
+	 */
 	public FileScan(File file) throws IOException, JSONException {
 		FileScan.fileToScan = file;
 		scanFile();
@@ -42,13 +73,26 @@ public class FileScan {
 
 	}
 
+	/**
+	 * get method for fileInfected var.This is for other programs to know
+	 * whether the file is infected or not
+	 * 
+	 * @param
+	 * @return fileInfected 	the boolean value of the infection status
+	 */
 	public boolean isFileInfected() {
 		return fileInfected;
 	}
 
-	private static final String URL_FILESCAN = "https://www.virustotal.com/vtapi/v2/file/scan";
-	private static final String URL_FILEREPORT = "https://www.virustotal.com/vtapi/v2/file/report";
-	private static final String API_KEY = "8eae5cfab4d8d18cd4a15b1aac7dbd5710558e2667a8394805160f819896a3b4";
+	/**
+	 * This method makes an api request through a HTTP POST request to send the
+	 * file to the api for scanning.Once the file is sent to the api for
+	 * scanning,the api returns a resource id.This is then stored as a variable
+	 * as above
+	 * 
+	 * @param
+	 * @return
+	 */
 
 	protected static void scanFile() throws IOException, JSONException {
 
@@ -73,6 +117,18 @@ public class FileScan {
 			client.close();
 		}
 	}
+
+	/**
+	 * This makes an api request through a HTTP POST request for getting the
+	 * file scan's results It passes in the API key anc the resource id which
+	 * set above Next with the results which was given by the api,the method
+	 * then counts the number of scan engines which reports that the file is
+	 * virus infected through the positives JSON tag If there is more than 0
+	 * positives,the file would then be determined to be infected with virus
+	 * 
+	 * @param
+	 * @return
+	 */
 
 	protected static void scanResults() throws IOException, JSONException {
 		CloseableHttpClient client = HttpClients.createDefault();
@@ -103,42 +159,23 @@ public class FileScan {
 		}
 
 	}
-
-	protected static void scanResultsTest() throws IOException, JSONException {
-		CloseableHttpClient client = HttpClients.createDefault();
-		HttpPost httpPost = new HttpPost(URL_FILEREPORT);
-		boolean infected = false;
-		int positives;
-		MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-		builder.addTextBody("apikey", API_KEY);
-		builder.addTextBody("resource", "8b7142fda0dd7bce9d76e6f561fbc5163bf4c0c8a81bc4391cd1e1bf7f940f1c");
-
-		HttpEntity multipart = builder.build();
-		httpPost.setEntity(multipart);
-
-		CloseableHttpResponse response1 = client.execute(httpPost);
-		try {
-			HttpEntity entity1 = response1.getEntity();
-			String responseBody = responseAsString(response1);
-			int responseStatus = response1.getStatusLine().getStatusCode();
-			JSONObject jObject = new JSONObject(responseBody); // json
-			positives = jObject.getInt("positives");
-			EntityUtils.consume(entity1);
-		} finally {
-			response1.close();
-			client.close();
-		}
-		if (positives > 0) {
-			FileScan.fileInfected = true;
-		} else {
-			FileScan.fileInfected = false;
-		}
-
-	}
-
+	/**
+	 * This method gets the HTTP response and converts it as a string
+	 * @param response	A closeableHTTPresponse is passed in for the conversion  
+	 * @return string	the converted string response      
+	 */
 	protected static String responseAsString(CloseableHttpResponse response) throws IOException {
 		return streamAsString(response.getEntity().getContent());
 	}
+	/**
+	 * This makes an api request through a HTTP POST request for getting the
+	 * file scan's results. It is essentially the same as the scanresults method.
+	 * However instead of checking the response,this first checks whether the response
+	 * status.If the response status is 0.It means the request rate for a minute has been met
+	 * and the program need to wait until it the file scanned results are out 
+	 * @param
+	 * @return
+	 */
 
 	protected static void checkResponseStatus() throws IOException, JSONException {
 		CloseableHttpClient client = HttpClients.createDefault();
@@ -163,7 +200,11 @@ public class FileScan {
 		}
 
 	}
-
+	/**
+	 * This method gets the InputStream and converts it to a string
+	 * @param inputStream	InputStream object for covnersion   
+	 * @return string	the converted InputStream    
+	 */
 	protected static String streamAsString(InputStream inputStream) throws IOException {
 		StringWriter writer = new StringWriter();
 		IOUtils.copy(inputStream, writer, "UTF-8");
