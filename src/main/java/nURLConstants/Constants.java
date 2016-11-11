e0f270f1d40dfefb38fcbededf770887115350ee
NThanatos <asuramirai@gmail.com>
2016-11-11 14:01:15 +0800

    https connection setup

--- src/main/java/nURLConstants/Constants.java
+++ src/main/java/nURLConstants/Constants.java
@@ -1,8 +1,8 @@
 package nURLConstants;
 
 public class Constants {
-//	private final static String mainURL = "https://localhost:8443/FreshdriveServer/";
-	private final static String mainURL = "http://localhost:8080/FreshdriveServer/";
+	private final static String mainURL = "https://localhost:8443/FreshdriveServer/";
+//	private final static String mainURL = "http://localhost:8080/FreshdriveServer/";
 	public final static String loginURL = mainURL + "Login";
 	public final static String logoutURL = mainURL + "Logout";
 	public final static String regURL = mainURL + "Register";
