dec9cd145e9934ab73e0bfe3f8dedcb3816d144d
Josey <josey@live.com.sg>
2016-11-12 18:44:06 +0800

    Code commenting + function fixes
    • Add or remove user permissions
    • Add multiple users at once
    • Removal of share button
    • Reject duplicate permissions
    • Thread sleep for a shorter time

--- src/main/java/nFile/FileController.java
+++ src/main/java/nFile/FileController.java
@@ -302,8 +302,17 @@ public class FileController implements Initializable {
 		}
 	}
 
+	/**
+	 * Switches screen to sharing screen.
+	 * Ensures that a file has been selected (an alert will be prompted otherwise).
+	 * Checks the ownership of the file, if current user is not the owner, a notification will be shown.
+	 * Moves to next screen, passing the selected file ID for to retrieve list of users who has access.
+	 * @param event
+	 * @throws IOException
+	 */
 	public void moveToShareScreen(ActionEvent event) throws IOException {
 		int selectedFile = fileListView.getSelectionModel().getSelectedIndex();
+		//Check if file is selected, else prompt alert
 		if (selectedFile == -1) {
 			Alert alert = new Alert(AlertType.INFORMATION);
 			alert.setTitle("Information");
@@ -317,17 +326,17 @@ public class FileController implements Initializable {
 				URL url = new URL(nURLConstants.Constants.ownershipURL);
 				HttpURLConnection con = (HttpURLConnection) url.openConnection();
 
-				// Adding Header
+				// Add header to request
 				con.setRequestMethod("POST");
 
-				// Send Post
+				// Post request to servlet
 				con.setDoOutput(true);
 				DataOutputStream out = new DataOutputStream(con.getOutputStream());
 				out.writeBytes("userID=" + account.get_id() + "&fileID=" + fileID);
 				out.flush();
 				out.close();
 
-				// Response from Server
+				// Response from servlet in boolean
 				BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 				String response;
 
@@ -343,7 +352,6 @@ public class FileController implements Initializable {
 					Parent root = (Parent) fxmlLoader.load();
 					ShareController controller = fxmlLoader.<ShareController> getController();
 					controller.setFileID(fileID);
-					System.out.println("Moving to ShareController");
 					Scene scene = new Scene(root);
 					Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 					app_stage.setScene(scene);
